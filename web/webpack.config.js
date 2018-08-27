1609d71da6aa19d9ad021a38f7250b1d04dfaa96
Alex Berry <berry3@indiana.edu>
2018-08-26 21:13:17 -0400

    adding webpack html plugin

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   module: {
@@ -13,5 +14,8 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
 };
